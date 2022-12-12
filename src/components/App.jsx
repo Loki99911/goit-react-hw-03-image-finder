38827d68d3b6811be434a41bfe317a413ad508a1
Loki99911 <nikita_kresik@i.ua>
2022-12-12 13:34:31 +0200

    14

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
 import { apiGet } from 'Service/api';
 // import { render } from '@testing-library/react';
 
@@ -11,18 +12,19 @@ export class App extends Component {
     imgArr: [],
     page: 1,
     totalImg: 0,
+    loaderOn: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const newName = this.state.name;
     const currentPage = this.state.page;
-console.log(this.state);
     if (prevState.name !== newName || prevState.page !== currentPage) {
+      this.setState({ loaderOn: true });
       apiGet(newName, currentPage).then(data => {
-        // console.log(data);
         this.setState(prevState => ({
           imgArr: [...prevState.imgArr, ...data.hits],
           totalImg: data.total,
+          loaderOn: false,
         }));
       });
     }
@@ -39,18 +41,23 @@ console.log(this.state);
   render() {
     return (
       <div
-      // style={{
-      // // height: '100vh',
-      // display: 'flex',
-      // justifyContent: 'center',
-      // alignItems: 'center',
-      // fontSize: 40,
-      // color: '#010101',
-      // }}
+        style={{
+          // // height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          fontSize: 40,
+          color: '#010101',
+        }}
       >
         <Searchbar onSubmitForm={this.handleSubmit} />
         <ImageGallery imgArr={this.state.imgArr} />
-        {this.state.imgArr.length < this.state.totalImg && <Button page={this.changePage} />}
+        {this.state.loaderOn && <Loader />}
+        {this.state.imgArr.length < this.state.totalImg && (
+          <Button page={this.changePage} />
+        )}
       </div>
     );
   }
